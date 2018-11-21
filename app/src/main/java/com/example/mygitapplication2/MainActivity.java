0df5edfea538eaaa6e034398516d21b1531cabec
yushuangping <3121009353@qq.com>
2018-11-21 10:08:51 +0800

    6666

--- app/src/main/java/com/example/mygitapplication2/MainActivity.java
+++ app/src/main/java/com/example/mygitapplication2/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.print("1");
-
+        System.out.print("2");
+        System.out.print("3");
     }
 }
